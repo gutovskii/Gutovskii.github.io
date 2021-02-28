9bcd654c16611aa6c0a823e9d05f6d53809bec0e
gutovskii <volodimir.gutovskiy@gmail.com>
2021-02-28 13:53:43 +0200

    Vue inputs 4
    Проверка по регулярному выражению

--- UI-library/my-input/my-input.js
+++ UI-library/my-input/my-input.js
@@ -1,10 +1,10 @@
 const app = {
 	data: () => ({
 		questions: [
-			{ title: "Как тебя зовут?", placeholder: "Иван" },
+			{ title: "Как тебя зовут?", placeholder: "Иван", type: "text", pattern: "^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$", errorMessage: "Введите имя без пробелов и лишних символов" },
             { title: "Сколько тебе лет?", placeholder: "15", type: "number" },
-            { title: "Твой email", placeholder: "ivan@example.com", required: true },
-            { title: "Фамилия", placeholder: "Логинов", required: true }
+            { title: "Твой email", placeholder: "ivan@example.com", type: "email", required: true, pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", errorMessage: "Неправильный e-mail" },
+            { title: "Фамилия", placeholder: "Логинов", type: "text", required: true, pattern: "^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$", errorMessage: "Введите фамилию буквами и без пробелов" }
 		],
         inputData: []
 	}),
@@ -14,10 +14,8 @@ const app = {
                 if (e.target.value.length == 0) e.target.classList.add('invalid-input')
                 else e.target.classList.remove('invalid-input')
             }
-        },
-        show(){
-            /*console.log(this.inputData)*/
-        }
+        }, 
+
     }
 }
 const newApp = Vue.createApp(app)
@@ -28,19 +26,34 @@ newApp.component('my-input', {
 		placeholder: String,
 		type: String,
         modelValue: String,
+        pattern: String,
+        errorMessage: String,
 		required: Boolean
 	},
     emits:['update:modelValue', 'blur'],
+    methods: {
+        checkStringByPattern(str, pattern){
+            if ( str !== undefined && pattern !== undefined && str.length > 0 ){
+                if ( str.search(pattern) == -1 ){
+                    return true
+                }
+                
+            }
+        } 
+    },
 	template: `
 	<div class="inputs-item">
         <label class="inputs-label">{{ required == true ? title + '*' : title }}</label>
-        <input class="inputs-input"
+        <div class="input-box">
+            <input class="inputs-input"
                 :value="modelValue"
                 @input="$emit('update:modelValue', $event.target.value)"
                 @blur="$emit('blur', $event)"
                 :placeholder="placeholder"
                 :type="type"
                 :required="required">
+            <span class="inputs-error" v-if="checkStringByPattern(modelValue, pattern)">{{ errorMessage }}</span>
+        </div>
     </div>
 	`
 })
